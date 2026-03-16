ed7c1259a5298e2ed13596b3b1ca3ded4e596a86
pottersteve <natasa.voicu.2006@gmail.com>
2026-03-16 23:01:20 +0200

    Add files via upload

---
+++ Scripts/GameOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+    public Text scoreText = null;
+    public Text killsText = null;
+
+    private void Awake()
+    {
+        scoreText.text = "Score: " + GameController.instance.playersScore.ToString();
+        killsText.text = "Demons killed: " + GameController.instance.playersKills.ToString();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
